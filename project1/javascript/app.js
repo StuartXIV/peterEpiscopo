fd5c19de6ea0a3aa32ef78425561f3c32b0002ef
Peter Episcopo <peteralbertstuart@hotmail.it>
2021-11-11 17:00:51 +0000

    topoMap

--- project1/javascript/app.js
+++ project1/javascript/app.js
@@ -35,10 +35,10 @@ function loading(){
 
 //MAP SETUP---------------------------------------------------------------------------------
 
-var map_dark = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+var map_dark = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
 	minZoom: 2,
 	maxZoom: 18,
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
     //apikey: '069cf193-4145-4c13-879a-83538c2c639c',
 	//attribution: '&copy; <&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
     });
