c08966ba3b514685beaf3289430ddfe882b11862
Peter Episcopo <peteralbertstuart@hotmail.it>
2022-01-03 17:53:21 +0000

    contact 1.2

--- assets/forms/form.php
+++ assets/forms/form.php
@@ -2,20 +2,18 @@
 
 <?php
 
-    if (isset($_POST['submit'])) {
-        $name = $_POST['name'];
-        $mail = $_POST['email'];        
-        $subject = $_REQUEST['subject'];
-        $message = $_POST['message'];
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $mail = $_POST['email'];
+    $message = $_POST['message'];
 
-        $mailTo = "peteralbertstuart@hotmail.it";
-        $headers = "From: contact@peterepiscopo.co.uk";
-        $txt = $message;
-        $content= "Message from " . ucfirst($name);
+    $mailTo = "peteralbertstuart@hotmail.it";
+    $headers = "From: contact@peterepiscopo.co.uk";
+    $txt = $message;
+    $content= "Message from " . ucfirst($name);
 
-        mail($mailTo, $content, $txt, $headers);
-        header("Location: ../../index.html");
-        
-    }
+    mail($mailTo, $content, $txt, $headers);
     
-    ?>
+}
+
+?>
