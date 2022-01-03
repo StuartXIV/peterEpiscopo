9f91097e17429ec7b0ea9e23e4e3af7ae8a875b8
Peter Episcopo <peteralbertstuart@hotmail.it>
2022-01-03 14:16:06 +0000

    New Home Page

---
+++ assets/forms/form.php
@@ -0,0 +1,21 @@
+
+
+<?php
+
+    if (isset($_POST['submit'])) {
+        $name = $_POST['name'];
+        $mail = $_POST['email'];        
+        $subject = $_REQUEST['subject'];
+        $message = $_POST['message'];
+
+        $mailTo = "peteralbertstuart@hotmail.it";
+        $headers = "From: contact@peterepiscopo.co.uk";
+        $txt = $message;
+        $content= "Message from " . ucfirst($name);
+
+        mail($mailTo, $content, $txt, $headers);
+        header("Location: ../../indexPeter.html#contact-form?mailsend");
+        
+    }
+    
+    ?>
