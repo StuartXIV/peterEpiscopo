848d0f92176ef01e064cb285d4e8287df99b6306
Peter Episcopo <peteralbertstuart@hotmail.it>
2022-01-18 14:25:30 +0000

    config fix

---
+++ rockdownBTS/php/config.php
@@ -0,0 +1,19 @@
+<?php
+
+	//$conn = new mysqli('localhost', 'root', 'Gilardino11!', 'u893668777_crud') or die(mysqli_error($conn));
+
+	//connection details for MySQL database
+
+	$cd_host = "localhost";
+	$cd_port = 3306;
+	$cd_socket = "";
+
+	// database name, username and password
+
+	$cd_dbname = "bts";
+	// $cd_user = "u893668777_peter";
+	$cd_user = "root";
+	$cd_password = "Gilardino11!";
+
+?>
+
