38810dc0feaf02b876df4880a426a2186daeccbf
Peter <peteralbertstuart@hotmail.it>
2024-02-06 11:57:57 +0000

    try new config

--- riptidesAvailability/php/config.php
+++ riptidesAvailability/php/config.php
@@ -9,17 +9,39 @@
 	$cd_port = 3306;
 	$cd_socket = "";
 
-	// database name, username and password
+	// database name, username and password'
+
+	
+	//// PROD
+	$cd_dbname = "";
+	$cd_user = "";
+	$cd_password = "";
+
+	
+	$current_url = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+
+	if ($current_url == "http://localhost/repos/peterEpiscopo/riptidesAvailability/php/config.php") {
+		$cd_dbname = "rip";
+		$cd_user = "root";
+		$cd_password = "Gilardino11!";
+	} else {
+		$cd_dbname = "u893668777_riptides";
+		$cd_user = "u893668777_rockup";
+		$cd_password = "Sixpence11!";
+	}
 
 	//// PROD
-	$cd_dbname = "u893668777_riptides";
-	$cd_user = "u893668777_rockup";
-	$cd_password = "Sixpence11!";
+	// $cd_dbname = "u893668777_riptides";
+	// $cd_user = "u893668777_rockup";
+	// $cd_password = "Sixpence11!";
 
 	//// TEST
 	// $cd_dbname = "rip";
 	// $cd_user = "root";
 	// $cd_password = "Gilardino11!";
+
+	// echo "The current URL is: " . $current_url;
+
 
 ?>
 
